8f268309f8a7ff0fc903a6c78a2dac7268d255d2
leandrotn <74495390+leandrotn@users.noreply.github.com>
2024-01-29 23:22:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,12 @@ const generateRandomString = (length) => {
 };
 
 const headers = {
-    'authority': 'api.discord.gx.games',
-    'accept': '*/*',
+    'authority': 'api.discord.gx.games'
     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
     'content-type': 'application/json',
     'origin': 'https://www.opera.com',
     'referer': 'https://www.opera.com/',
     'sec-ch-ua': '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'cross-site',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0'
 };
 
